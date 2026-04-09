0d5ab2523ce0e4c05fa7c2bbd331c84cc519636e
buyath <buyaththayub@gmail.com>
2026-04-09 16:52:59 +0000

    day 2 how javascript works done

---
+++ day2/practice.js
@@ -0,0 +1,10 @@
+let num = 2;
+function square(num){
+    let ans  = num * num;
+    return ans;
+}
+let square2 = square(num);
+let square4 = square(4);
+console.log(square2);
+console.log(square4);
+console.log(square);
