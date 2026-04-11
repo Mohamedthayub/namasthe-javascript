c9581fddc74dafa28dd1f86da0cdd22ed57fa4dc
buyath <buyaththayub@gmail.com>
2026-04-11 08:23:08 +0000

    hoisting done

---
+++ day3/practice.js
@@ -0,0 +1,21 @@
+
+console.log(x);
+var x  = "this is for test";
+getName();
+
+function getName(){
+    console.log("This is for test");
+}
+
+
+/*
+🔥 Simple Definition (Interview Ready)
+
+Hoisting is JavaScript’s default behavior of moving declarations (variables and functions) to the top of their scope during the compilation phase.
+
+execution
+
+🔥 Better Definition (Fix this in your mind):
+
+Hoisting is JavaScript’s behavior of moving declarations to the top of their scope, before execution.
+*/
