7cb8a9726f7f4dbe36f600d85fcd1e28812e385e
buyath <buyaththayub@gmail.com>
2026-05-07 16:55:01 +0000

    today i have learnt about scope chain and lexical environment

---
+++ day7/practice.js
@@ -0,0 +1,16 @@
+function a(){
+    function c(){
+        console.log(b);    
+    }
+    c();
+};
+var b = "Hello";
+a();
+
+
+/*
+in this episode we are going to learn about scope chain
+
+what will happen when i access  the variable  outer the function ? 
+
+*/
